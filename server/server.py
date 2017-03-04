651520e34aef2ec4d1242d6b7c2d1d6d90838bc1
ad5915 <dan.alex97@yahoo.com>
2017-03-04 06:16:16 +0100

    Changed to a single thread.

--- server/server.py
+++ server/server.py
@@ -18,27 +18,46 @@ host = socket.gethostname()
 
 serversocket.bind((host, port))
 
-class client(Thread):
-    def __init__(self, socket, address):
-        Thread.__init__(self)
-        self.sock = socket
-        self.addr = address
-        self.start()
+# class client(Thread):
+#     def __init__(self, socket, address):
+#         Thread.__init__(self)
+#         self.sock = socket
+#         self.addr = address
+#         self.start()
+#
+#     def run(self):
+#         engine = Engine(self.sock, self.addr)
+#         max_input = 1024
+#
+#         while 1:
+#             data = self.sock.recv(max_input)
+#
+#             engine.receive(data)
+#             self.sock.send(engine.response())
 
-    def run(self):
-        engine = Engine(self.sock, self.addr)
-        max_input = 1024
-
-        while 1:
-            data = self.sock.recv(max_input).decode()
-
-            engine.receive(data)
-            self.sock.send(engine.response())
-
-capacity = 5
-serversocket.listen(capacity)
+# capacity = 5
+serversocket.listen(1)
 
 print ('Server started and listening.')
 while True:
-    clientsocket, address = serversocket.accept()
-    client(clientsocket, address)
+	csock, caddr = serversocket.accept()
+
+	print "Connection from: " + `caddr`
+	req = csock.recv(1024)
+
+	lines = req.split('\n')
+
+	csock.sendall("""HTTP/1.0 200 OK
+		Content-Type: text/html
+		<html>
+			<head>
+				<title>Success</title>
+			</head>
+			<body>
+				Success
+			</body>
+		</html>
+	""")
+
+	#clientsocket, address = serversocket.accept()
+    #client(clientsocket, address)
