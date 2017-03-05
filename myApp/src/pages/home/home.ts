02be8ec2949853e1896bcee452541b73a4abe43a
Andrei Isaila <ii515@ic.ac.uk>
2017-03-05 02:01:50 +0000

    Working front-end, picture size 256x256. (maybe too big)

--- myApp/src/pages/home/home.ts
+++ myApp/src/pages/home/home.ts
@@ -11,6 +11,12 @@ import {
   Camera
 } from 'ionic-native';
 
+import {
+  Http
+} from '@angular/http';
+
+import { NavController, NavParams } from 'ionic-angular';
+import { CurrentUserService } from '../../providers/current_user';
 @Component({
   templateUrl: 'home.html'
 })
@@ -18,27 +24,43 @@ import {
 export class HomePage {
   private base64Image: string;
   private pic_taken: string;
+  public myhttp: Http;
 
   constructor(
     public platform: Platform,
-    public actionsheetCtrl: ActionSheetController
+    public actionsheetCtrl: ActionSheetController,
+    public http: Http
   ) {
+    this.myhttp = http;
     this.pic_taken = "false";
+    this.base64Image = "batman";
   }
 
   takePicture(){
      Camera.getPicture({
          destinationType: Camera.DestinationType.DATA_URL,
-         targetWidth: 1000,
-         targetHeight: 1000
+         targetWidth: 256,
+         targetHeight: 256
      }).then((imageData) => {
          this.pic_taken = "true";
-         this.base64Image = "data:image/jpeg;base64," + imageData;
+         this.base64Image = "data:image/jpeg;base64," +imageData;
      }, (err) => {
         this.pic_taken = err;
         console.log(this.pic_taken);
      });
    }
+
+
+postPicture() {
+    var data = this.base64Image;
+    var link = "https://kidappservice.scalingo.io/";
+    link = link + data;
+    this.myhttp.get(String(link)).subscribe(response => {
+      response = JSON.parse(response['_body'])
+    }, err => {
+      console.log(err);
+    });
+}
 
 openMenu() {
     let actionSheet = this.actionsheetCtrl.create({
